729eb7075494449934b80790c0930a8906b90d76
usuario <usuario@MacBook-Pro-de-usuario.local>
2021-04-27 23:42:07 +0200

    Bola con diccionario

---
+++ ball.py
@@ -0,0 +1,65 @@
+import pygame as pg
+import sys
+from random import randint
+
+def rebotaX(x):
+    if x <=0 or x >=ANCHO:
+        return -1
+
+    return 1
+
+def rebotaY(y):
+    if y <=0 or y >=ALTO:
+        return -1
+
+    return 1
+
+
+ROJO = (255, 0, 0)
+AZUL = (0, 0, 255)
+VERDE = (0, 255, 0)
+NEGRO = (0, 0, 0)
+ANCHO = 800
+ALTO = 600
+
+pg.init()
+pantalla = pg.display.set_mode((ANCHO, ALTO)) #Creacion de la pantallaa
+reloj = pg.time.Clock() # Controlador de FPS
+
+bolas = []
+for _ in range(10): #Crear 10 bolas
+    bola = {'x': randint(0, ANCHO),
+            'y': randint(0, ALTO),
+            'vx': randint(5, 10),
+            'vy': randint(5, 10),
+            'color': (randint(0, 255), randint(0,255), randint(0,255))
+    }
+    bolas.append(bola)
+
+game_over = False
+while not game_over:
+    v = reloj.tick(60)
+    #Gestion de eventos
+    for evento in pg.event.get():
+        if evento.type == pg.QUIT:
+            game_over = True
+
+    # Modificación de estado
+    for bola in bolas:
+        bola['x'] += bola['vx']
+        bola['y'] += bola['vy']
+
+        bola['vy'] *= rebotaY(bola['y'])
+        bola['vx'] *= rebotaX(bola['x'])
+
+
+    # Gestión de la pantalla
+    pantalla.fill(NEGRO) #Recolocar pantalla
+    for bola in bolas:
+        pg.draw.circle(pantalla, bola['color'], (bola['x'], bola['y']), 10) #Creacion de la bola
+
+
+    pg.display.flip() #Refrescar pantalla
+
+pg.quit()
+sys.exit()
