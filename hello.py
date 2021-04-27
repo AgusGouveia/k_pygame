d27787c85d6b1735f133480a6ca12959f45160a0
usuario <usuario@MacBook-Pro-de-usuario.local>
2021-04-27 21:01:13 +0200

    Final de hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
 import pygame as pg
+import sys
+
+def fin_juego():
+    pg.quit()
+    sys.exit()
 
 pg.init()
 pantalla = pg.display.set_mode((600, 400)) #Creacion de la pantalla
@@ -9,12 +14,16 @@ game_over = False
 while not game_over:
     #Gestion de eventos
     for evento in pg.event.get():
-        pass
+        if evento.type == pg.QUIT:
+            game_over = True #Colocar la salida del bucle colocando el True o tambien seria posible colocnado el fin_juego
+
 
     #Gestion del estado
     print('Hola mundo')
 
     #Refrescar la pantalla
     pantalla.fill((0, 255, 0)) #Color de pantalla
-
     pg.display.flip() #Literalmente refrescar la pantalla. Indispensable.
+
+
+fin_juego()
