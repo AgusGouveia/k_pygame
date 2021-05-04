1791ee69631f41294688569d080417cdb3c2daa4
usuario <usuario@MacBook-Pro-de-usuario.local>
2021-05-04 22:25:30 +0200

    Bola con esteroides. Falta texto en pantalla

--- ball3.py
+++ ball3.py
@@ -1,20 +1,6 @@
 import pygame as pg
 import sys
-from random import randint
-
-def rebotaX(x):
-    if x <=0 or x >=ANCHO:
-        return -1
-
-    return 1
-
-def rebotaY(y):
-    if y <=0 or y >=ALTO:
-        return -1
-
-    return 1
-
-
+from random import randint, choice
 
 ROJO = (255, 0, 0)
 AZUL = (0, 0, 255)
@@ -24,59 +10,107 @@ ANCHO = 800
 ALTO = 600
 
 pg.init()
-pantalla = pg.display.set_mode((ANCHO, ALTO)) #Creacion de la pantalla
-reloj = pg.time.Clock() # Controlador de FPS
+pantalla = pg.display.set_mode((ANCHO, ALTO))
+reloj = pg.time.Clock()
+
+fuenteSys = pg.font.SysFont("segoe print", 30)
+textoSys = fuenteSys.render("Vidas: {}", True, VERDE).format(vidas)
 
 class Bola():
-    def __init__(self, x, y, vx, vy, color):
+    def __init__(self, x, y, vx=5, vy=5, color= (255, 255, 255), radio=10):
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
         self.color = color
+        self.anchura = radio*2
+        self.altura = radio*2
 
-    def randdir():
-        x = randint(0,1)
-        if x == 0:
-            y = randint(-10, -5)
-        else:
-            y = randint(5, 10)
-        return y
+    def actualizar(self):
+        self.x += self.vx
+        self.y += self.vy
+        if self.y <=0:
+            self.vy = -self.vy
+        if self.x <=0 or self.x >=ANCHO:
+            self.vx = -self.vx
+        if self.y >=ALTO:
+            self.x = ANCHO // 2
+            self.y = ALTO // 2
+            self.vx = randint(5,10)*choice([-1, 1])
+            self.vy = randint(5,10)*choice([-1, 1])
+            return True
+        return False
 
-bolas = []
-for _ in range(20): #Crear 10 bolas con valores randoms (randint)
-    bola = Bola(randint(0, ANCHO),
-                randint(0, ALTO),
-                Bola.randdir(),
-                Bola.randdir(),
-                (randint(0, 255), randint(0,255), randint(0,255)))
+    def dibujar(self, lienzo):
+        pg.draw.circle(lienzo, self.color, (self.x, self.y), self.anchura//2)
+    
+    def comprueba_colision(self, objeto):
+        choqueX = self.x >= objeto.x and self.x <= objeto.x+objeto.anchura or \
+           self.x+self.anchura >= objeto.x and self.x+self.anchura <= objeto.x + objeto.anchura
+        choqueY = self.y >= objeto.y and self.y <= objeto.y+objeto.altura or \
+           self.y+self.altura >= objeto.y and self.y+self.altura <= objeto.y + objeto.altura
+        
+        if choqueX and choqueY:
+            self.vy *= -1
 
-    bolas.append(bola)
+class Raqueta():
+    def __init__(self, x=0, y=0):
+        self.altura = 10
+        self.anchura = 100
+        self.color = (255, 255, 255)
+        self.x = (ANCHO - self.anchura) // 2
+        self.y = ALTO - self.altura - 15
+        self.vy = 0
+        self.vx = 13
 
+    def dibujar(self, lienzo):
+        rect = pg.Rect(self.x, self.y, self.anchura, self.altura)
+        pg.draw.rect(lienzo, self.color, rect)
+    
+    def actualizar(self):
+        teclas_pulsadas = pg.key.get_pressed() 
+        if teclas_pulsadas[pg.K_LEFT] and self.x > 0:
+            self.x -= self.vx
+        if teclas_pulsadas[pg.K_RIGHT] and self.x < ANCHO - self.anchura:
+            self.x += self.vx
+
+vidas = 3
+
+bola = Bola(randint(0, ANCHO),
+            randint(0, ALTO),
+            randint(5, 10)*choice([-1, 1]),
+            randint(5, 10)*choice([-1, 1]),
+            AZUL)
+
+raqueta = Raqueta()
 game_over = False
-while not game_over:
-    v = reloj.tick(60)
+while not game_over and vidas > 0:
+    v = reloj.tick(40)
     #Gestion de eventos
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
 
     # Modificación de estado
-    for bola in bolas:
-        bola.x += bola.vx
-        bola.y += bola.vy
-
-        bola.vy *= rebotaY(bola.y)
-        bola.vx *= rebotaX(bola.x)
-
+    raqueta.actualizar()
+    pierdebola = bola.actualizar()
+    bola.comprueba_colision(raqueta)
 
     # Gestión de la pantalla
-    pantalla.fill(NEGRO) #Recolocar pantalla
-    for bola in bolas:
-        pg.draw.circle(pantalla, bola.color, (bola.x, bola.y), 10) #Creacion de la bola
-
-
-    pg.display.flip() #Refrescar pantalla
+    pantalla.fill(NEGRO)
+    pantalla.blit(textoSys,(20, 20))
+    bola.dibujar(pantalla)
+    raqueta.dibujar(pantalla)
+    pg.display.flip()
+    def contadorVidas():
+        if pierdebola:
+            vidas -= 1
+            v = str(vidas)
+            fuenteSys2 = pg.font.SysFont("segoe print", 30)
+            textoSys2 = fuenteSys2.render('v', True, ROJO)
+            pantalla.blit(textoSys2,(50, 20))
+    if pierdebola:
+        pg.time.delay(600)
 
 pg.quit()
 sys.exit()
